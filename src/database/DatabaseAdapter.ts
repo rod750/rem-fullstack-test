b863ed9685311a311a5c87356995c4d74c053390
Rodrigo D. Cuervo <rod750@gmail.com>
2022-08-03 21:47:00 -0500

    feat: added additional methods to database adapter interface

--- src/database/DatabaseAdapter.ts
+++ src/database/DatabaseAdapter.ts
@@ -2,8 +2,16 @@ import { Team } from '@/database/models/team';
 import { Trainer } from '@/database/models/trainer';
 
 export interface DatabaseAdapter {
-  getTrainers(): Trainer[];
-  getTrainer(trainerId: string): Trainer;
-  getTeams(trainerId: string): Team[];
-  getTeam(teamId: string): Team;
+  getTrainers(): Promise<Trainer[]>;
+  getTrainer(trainerId: string): Promise<Trainer>;
+  getTrainerByCredentials(name: string, password: string): Promise<Trainer>;
+  createTrainer(trainer: Trainer): Promise<boolean>;
+  updateTrainer(trainer: Trainer): Promise<boolean>;
+  deleteTrainer(trainerId: string): Promise<boolean>;
+  getTeams(trainerId: string): Promise<Team[]>;
+  getTeam(teamId: string): Promise<Team>;
+  createTeam(team: Team): Promise<boolean>;
+  updateTeam(team: Team): Promise<boolean>;
+  deleteTeam(teamId: string): Promise<boolean>;
+  connect(url: string): Promise<void>;
 }
